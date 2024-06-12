51c2952e06880aa2c63e960cbcf31d746cdaa18f
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2024-06-12 23:36:10 +0530

    Time: 37 ms (45.95%), Space: 43.5 MB (99.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,23 +2,24 @@ class Solution {
     public static int[] twoSum(int[] nums, int target) {
         int n=nums.length;
         int[] ans=new int[2];
-        HashMap<Integer,Integer> mpp=new HashMap<>();
         ans[0]=ans[1]=-1;
         for(int i=0;i<n;i++){
-            int curr=nums[i];
-            int moreNeed=target-curr;
-            if(mpp.containsKey(moreNeed)){
-                ans[0]=mpp.get(moreNeed);
-                ans[1]=i;
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
+                }
             }
-            mpp.put(nums[i],i);
         }
-        return ans;
+      return ans;
+        
     }
     public static void main(String args[]){
-        int[] nums={2,7,11,15};
-        int target=9;
+        int[] nums={2,6,5,8,11};
+        int target=14;
         int[] ans=twoSum(nums,target);
         System.out.println("index:["+ans[0]+","+ans[1]+"]");
+
     }
 }
