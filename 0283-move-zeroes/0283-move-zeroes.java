7e35f7e767ea84bd70e20607ae0772a8e76ae3be
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2024-06-12 23:39:56 +0530

    Time: 147 ms (5.04%), Space: 46.4 MB (8.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,25 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int j=-1;
-       int n=nums.length;
-       for(int i=0;i<n;i++){
-           if(nums[i]==0){
-               j=i;
-               break;
-           }
-       }
-       if(j!=-1){
-       for(int i=j+1;i<n;i++){
-           if(nums[i]!=0){
-               int temp=nums[i];
-               nums[i]=nums[j];
-               nums[j]=temp;
-               j++;
-           }
-       }
-       }
-       for(int i=0;i<n;i++){
-           System.out.print(nums[i]+" ");
-       }
+        int n=nums.length;
+        ArrayList<Integer> temp = new ArrayList<>();
+        // copy non-zero elements
+        // from original -> temp array:
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0)
+                temp.add(nums[i]);
+        }
+
+        // copy elements from temp
+        // fill first nz fields of
+        // original array:
+        for (int i = 0; i < temp.size(); i++) {
+            nums[i] = temp.get(i);
+        }
+
+        // fill rest of the cells with 0:
+        for (int i = temp.size(); i < n; i++) {
+            nums[i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
