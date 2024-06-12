6fbb17268fa918fa5620013de4a311907c29c574
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2024-06-12 23:36:02 +0530

    Time: 5 ms (57.14%), Space: 43.4 MB (99.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,24 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        if (nums.length <= 1) return null;
-        if (nums.length == 2) {
-            if (nums[0] + nums[1] == target) {
-                return new int[]{0, 1};
+    public static int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        int[] ans=new int[2];
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        ans[0]=ans[1]=-1;
+        for(int i=0;i<n;i++){
+            int curr=nums[i];
+            int moreNeed=target-curr;
+            if(mpp.containsKey(moreNeed)){
+                ans[0]=mpp.get(moreNeed);
+                ans[1]=i;
             }
-            return null;
+            mpp.put(nums[i],i);
         }
-        int pointer1 = 0, pointer2 = pointer1 + 1;
-        while (nums.length > pointer1) {
-            if (pointer2 >= nums.length) {
-                pointer2 = ++pointer1 + 1;
-                continue;
-            }
-            if (nums[pointer1] + nums[pointer2] == target) {
-                return new int[]{pointer1, pointer2};
-            }
-            pointer2++;
-        }
-        return null;
+        return ans;
+    }
+    public static void main(String args[]){
+        int[] nums={2,7,11,15};
+        int target=9;
+        int[] ans=twoSum(nums,target);
+        System.out.println("index:["+ans[0]+","+ans[1]+"]");
     }
 }
