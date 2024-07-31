9d8c940b163b987211d7d2fa3f5d98e1376dba1b
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2024-07-31 16:41:58 +0530

    Time: 0 ms (100%), Space: 45.1 MB (64.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int N = nums.length;
+        int N = nums.length;//3
         int xor1 = 0,xor2=0;
         for(int i=0;i<N;i++){
-            xor2 = xor2 ^ nums[i];
-            xor1 = xor1 ^ (i+1);
+            xor2 = xor2 ^ nums[i];//0^3^0^1
+            xor1 = xor1 ^ (i+1);//0^1^2^3
+            /* Don't bother about o/p in each step we anyway don't want that no.Even if some no. forms at the end of xor1 & xor2 pc would have it*/
         }
+        /*(0^3^0^1)^(0^1^2^3) = (0^0)^(3^3)^(1^1)^(0^2) = (0^0)^(0^2) = 0^2 = 2 ie,a^a=0 and a^0=0^a=a (order doesn't matter as all are ^ ).*/
         return (xor1 ^ xor2);
     }
 }
